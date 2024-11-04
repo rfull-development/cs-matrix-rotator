a7906af6b0c21bc18672cc686d8a116be0ce6304
Ryota Funaki <contact@ngv.jp>
2024-11-04 22:28:35 +0900

    Exit codes improved (#10)
    This pull request introduces an `ExitCode` class to standardize exit
    codes and improves error handling in the `Program.cs` file. The changes
    include defining success and failure exit codes and updating the program
    to return these codes appropriately upon completion or error.
    
    Standardizing exit codes:
    
    *
    [`ExitCode.cs`](diffhunk://#diff-2180b6788439ee268303ddd0489a69f6c9946b695681ed7c6d5637d48669f50aR1-R10):
    Added a new class `ExitCode` with constants for `Success` and `Failure`
    exit codes.
    
    Error handling improvements:
    
    *
    [`Program.cs`](diffhunk://#diff-0b69b473fe937040615d69f606751f61ddbc2e3a1849360ff2456c22afe88c0bL12-R12):
    Updated the usage message return statement to use `ExitCode.Failure`.
    *
    [`Program.cs`](diffhunk://#diff-0b69b473fe937040615d69f606751f61ddbc2e3a1849360ff2456c22afe88c0bR21-R65):
    Added try-catch blocks around file loading, matrix rotation, and file
    saving operations to handle exceptions and return `ExitCode.Failure` on
    errors.

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ if (string.IsNullOrEmpty(StartupParameter.InputPath) ||
 {
     Console.Error.WriteLine("Usage:");
     Console.Error.WriteLine("  MatrixRotator <--input|-i> input path <--output|-o> output path");
-    return;
+    return ExitCode.Failure;
 }
 
 Stopwatch stopwatch = new();
@@ -18,12 +18,48 @@ stopwatch.Start();
 Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 var encoding = Encoding.GetEncoding("shift_jis");
 Editor editor = new();
-using StreamReader reader = new(StartupParameter.InputPath, encoding);
-editor.Load(reader);
-editor.Rotate();
-using StreamWriter writer = new(StartupParameter.OutputPath, false, encoding);
-editor.Save(writer);
+
+Console.Write($"Inputfile '{StartupParameter.InputPath}' loading ... ");
+try
+{
+    using StreamReader reader = new(StartupParameter.InputPath, encoding);
+    editor.Load(reader);
+}
+catch (Exception e)
+{
+    Console.Error.WriteLine($"Failed to load the input file: {StartupParameter.InputPath}");
+    Console.Error.WriteLine($"Description: {e.Message}");
+    return ExitCode.Failure;
+}
+Console.WriteLine("Done.");
+
+Console.Write("Rotating the matrix ... ");
+try
+{
+    editor.Rotate();
+}
+catch (Exception e)
+{
+    Console.Error.WriteLine($"Description: {e.Message}");
+    return ExitCode.Failure;
+}
+Console.WriteLine("Done.");
+
+Console.Write($"Outputfile '{StartupParameter.OutputPath}' saving ... ");
+try
+{
+    using StreamWriter writer = new(StartupParameter.OutputPath, false, encoding);
+    editor.Save(writer);
+}
+catch (Exception e)
+{
+    Console.Error.WriteLine($"Failed to save the output file: {StartupParameter.OutputPath}");
+    Console.Error.WriteLine($"Description: {e.Message}");
+    return ExitCode.Failure;
+}
+Console.WriteLine("Done.");
 
 stopwatch.Stop();
 double processingTime = stopwatch.Elapsed.TotalSeconds;
 Console.Out.WriteLine($"Done(processing time: {processingTime} seconds)");
+return ExitCode.Success;
