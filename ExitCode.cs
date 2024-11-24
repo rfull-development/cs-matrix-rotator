f591c413b20372df1c1286db95ed606dba3d2f13
Ryota Funaki <contact@ngv.jp>
2024-11-24 16:52:01 +0900

    Implementation (#2)
    This pull request introduces significant changes to the Matrix Rotator
    project, including the addition of new files, updates to existing
    configurations, and the removal of obsolete scripts. Below is a summary
    of the most important changes:
    
    ### New Features and Enhancements:
    
    * **MatrixRotator Project Initialization:**
    - Added `MatrixRotator.csproj` to define the project structure and
    dependencies.
    - Added `MatrixRotator.sln` to include the main project and unit test
    project.
    
    * **Core Functionality:**
    - Implemented the `Editor` class to handle loading, saving, and rotating
    matrix data.
    - Added `Program.cs` to handle command-line input and execute the matrix
    rotation process.
    - Introduced `ExitCode` class to define standard exit codes for the
    application.
    - Added `StartupParameter` class to parse and store command-line
    arguments.
    
    ### Configuration and Workflow Updates:
    
    * **GitHub Actions Workflows:**
    - Added `01_commit.yml` for commit checks, including build verification.
    - Added `02_review.yml` for review checks, including static analysis and
    unit tests.
    
    * **Editor Configuration:**
      - Removed SQL-specific settings from `.editorconfig`.
      - Updated `.gitattributes` to remove database file handling.
    
    ### Documentation:
    
    * **README Update:**
    - Updated `README.md` to reflect the new project name and usage
    instructions.
    
    ### Cleanup:
    
    * **Obsolete Scripts Removal:**
    - Removed `install.sh` and related scripts from `.devcontainer/shells/`.
    [[1]](diffhunk://#diff-b1585b82e892fd36a02245c80aa27ed509338eb3b2a1a45fda33ddf3101b1dddL1-L14)
    [[2]](diffhunk://#diff-10f7e53ce4ec1ace47578b0f60c60f30d04e579d16e4ec6b4b357710d5b6af58L1-L15)
    - Updated `.devcontainer/devcontainer.json` to remove the
    `postCreateCommand`.
    
    ### Unit Testing:
    
    * **Unit Tests:**
    - Added `EditorTests.cs` to test the core functionality of the `Editor`
    class.
    - Added `UnitTest.csproj` to define the unit test project and its
    dependencies.

---
+++ ExitCode.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) 2024 RFull Development
+// This source code is managed under the MIT license. See LICENSE in the project root.
+namespace MatrixRotator
+{
+    public class ExitCode
+    {
+        public const int Success = 0;
+        public const int Failure = 1;
+    }
+}
---
+++ Tests/UnitTest/EditorTests.cs
@@ -0,0 +1,133 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace MatrixRotator.Tests
+{
+    [TestClass]
+    [TestCategory("Unit")]
+    public class EditorTests
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+        }
+
+        [TestMethod]
+        public void Load()
+        {
+            int rowCount = 4;
+            int columnCount = 3;
+            string input = GenerateCsvString(rowCount, columnCount);
+            byte[] rawInput = Encoding.UTF8.GetBytes(input);
+            using var stream = new MemoryStream(rawInput);
+            using var reader = new StreamReader(stream);
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var editor = new Editor();
+            editor.Load(reader);
+
+            stopwatch.Stop();
+            double processingTime = stopwatch.Elapsed.TotalSeconds;
+            Console.WriteLine($"Processing time: {processingTime} seconds.");
+
+            for (int rowIndex = 0; rowIndex < rowCount; rowIndex++)
+            {
+                for (int columnIndex = 0; columnIndex < columnCount; columnIndex++)
+                {
+                    string expected = $"{columnCount * rowIndex + columnIndex + 1}";
+                    Assert.AreEqual(expected, editor.Data[rowIndex, columnIndex]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Save()
+        {
+            int rowCount = 4;
+            int columnCount = 3;
+            string input = GenerateCsvString(rowCount, columnCount);
+            byte[] rawInput = Encoding.UTF8.GetBytes(input);
+            var editor = new Editor();
+            using var inputStream = new MemoryStream(rawInput);
+            using (var reader = new StreamReader(inputStream))
+            {
+                editor.Load(reader);
+            }
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            using var outputStream = new MemoryStream();
+            using (var writer = new StreamWriter(outputStream))
+            {
+                editor.Save(writer);
+                writer.Flush();
+
+                stopwatch.Stop();
+                double processingTime = stopwatch.Elapsed.TotalSeconds;
+                Console.WriteLine($"Processing time: {processingTime} seconds.");
+
+                outputStream.Position = 0;
+                using var reader = new StreamReader(outputStream);
+                string output = reader.ReadToEnd();
+                Assert.AreEqual(input, output);
+            }
+        }
+
+        [TestMethod]
+        public void Rotate()
+        {
+            int rowCount = 4;
+            int columnCount = 3;
+            string input = GenerateCsvString(rowCount, columnCount);
+            byte[] rawInput = Encoding.UTF8.GetBytes(input);
+            using var stream = new MemoryStream(rawInput);
+            using var reader = new StreamReader(stream);
+            var editor = new Editor();
+            editor.Load(reader);
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            editor.Rotate();
+
+            stopwatch.Stop();
+            double processingTime = stopwatch.Elapsed.TotalSeconds;
+            Console.WriteLine($"Processing time: {processingTime} seconds.");
+
+            for (int columnIndex = 0; columnIndex < columnCount; columnIndex++)
+            {
+                for (int rowIndex = 0; rowIndex < rowCount; rowIndex++)
+                {
+                    string expected = $"{columnCount * rowIndex + columnIndex + 1}";
+                    Assert.AreEqual(expected, editor.Data[columnIndex, rowIndex]);
+                }
+            }
+        }
+
+        private static string GenerateCsvString(int rowCount, int columnCount)
+        {
+            StringBuilder builder = new();
+            for (int rowIndex = 0; rowIndex < rowCount; rowIndex++)
+            {
+                string[] row = new string[columnCount];
+                for (int columnIndex = 0; columnIndex < columnCount; columnIndex++)
+                {
+                    row[columnIndex] = $"{columnCount * rowIndex + columnIndex + 1}";
+
+                }
+                string line = string.Join(",", row);
+                builder.AppendLine(line);
+            }
+            return builder.ToString();
+        }
+    }
+}
